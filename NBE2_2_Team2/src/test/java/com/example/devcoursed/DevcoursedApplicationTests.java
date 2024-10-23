7efc9c5b0a8c2c897f6e2dae5e489428b16f9e76
asklas <asklas+jyjy0372@gmail.com>
2024-10-23 14:47:25 +0900

    project2

---
+++ NBE2_2_Team2/src/test/java/com/example/devcoursed/DevcoursedApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.devcoursed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DevcoursedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
